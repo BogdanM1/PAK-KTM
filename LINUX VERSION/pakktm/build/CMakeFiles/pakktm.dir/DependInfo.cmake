dc912ac0edb32f8282875321bd2dbcda8adcc0b5
BogdanM1 <dd11eeaann@gmail.com>
2023-06-29 17:26:16 +0200

    linux version added

---
+++ LINUX VERSION/pakktm/build/CMakeFiles/pakktm.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/bogdan/pakktm/src/libseq/elapse.c" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/libseq/elapse.c.o"
+  "/home/bogdan/pakktm/src/libseq/mpic.c" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/libseq/mpic.c.o"
+  "/home/bogdan/pakktm/src/sparse_assembler/sparse_assembler.c" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/sparse_assembler/sparse_assembler.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+SET(CMAKE_DEPENDS_CHECK_Fortran
+  "/home/bogdan/pakktm/src/libseq/mpi.f" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/libseq/mpi.f.o"
+  "/home/bogdan/pakktm/src/pak/Mumps.for" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/Mumps.for.o"
+  "/home/bogdan/pakktm/src/pak/PAK00T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK00T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK01T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK01T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK02T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK02T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK03T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK03T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK11T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK11T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK21T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK21T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/PAK31T.FOR" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/PAK31T.FOR.o"
+  "/home/bogdan/pakktm/src/pak/xerbla.for" "/home/bogdan/pakktm/build/CMakeFiles/pakktm.dir/src/pak/xerbla.for.o"
+  )
+SET(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/MUMPS"
+  "../src/libseq"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ LINUX VERSION/pakktm/build/CMakeFiles/pakktm.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pakktm.dir/src/pak/xerbla.for.o"
+  "CMakeFiles/pakktm.dir/src/pak/Mumps.for.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK03T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK21T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK11T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK00T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK02T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK01T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/pak/PAK31T.FOR.o"
+  "CMakeFiles/pakktm.dir/src/libseq/mpi.f.o"
+  "CMakeFiles/pakktm.dir/src/libseq/mpic.c.o"
+  "CMakeFiles/pakktm.dir/src/libseq/elapse.c.o"
+  "CMakeFiles/pakktm.dir/src/sparse_assembler/sparse_assembler.c.o"
+  "pakktm.pdb"
+  "pakktm"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C Fortran)
+  INCLUDE(CMakeFiles/pakktm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
